f848668c52daf37b4f18a6696a5ea75f6e24e0ee
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-08 22:30:16 +0600

    active badge added

--- components/Dashboard/Header/index.tsx
+++ components/Dashboard/Header/index.tsx
@@ -1,4 +1,6 @@
-import { BiSearch } from "react-icons/bi";
+import { BiSearch, BiUserCircle } from "react-icons/bi";
+import { HiUserCircle } from "react-icons/hi";
+import { MdNotificationsNone as NotificationIcon } from "react-icons/md";
 
 export default function DashboardHeader() {
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
@@ -11,7 +13,7 @@ export default function DashboardHeader() {
       <h1 className="text-2xl text-gray-900 font-semibold">Dashboard</h1>
 
       {/* right part of the header */}
-      <div className="ml-auto">
+      <div className="ml-auto flex items-center gap-4">
         {/* Search field */}
         <form
           onSubmit={handleSubmit}
@@ -29,6 +31,16 @@ export default function DashboardHeader() {
             <BiSearch />
           </button>
         </form>
+
+        {/* Notification Icon */}
+        <button className="text-[29px] relative">
+          <NotificationIcon />
+          <span className="absolute top-[1px] right-[1px] w-[8px] h-[8px] rounded-xl bg-red-500" />
+        </button>
+        <button className="text-[33px] relative">
+          <BiUserCircle />
+          <span className="absolute top-[1px] right-[1px] w-[10px] h-[10px] rounded-xl bg-green-600 border-2 border-white" />
+        </button>
       </div>
     </header>
   );
