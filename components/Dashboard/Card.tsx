4ce89af1514d8cbecbebbda6808f21d0a3d76cbb
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-08 23:47:22 +0600

    add card button added

---
+++ components/Dashboard/Card.tsx
@@ -0,0 +1,6 @@
+import overlay from "@/assets/images/card-overlay-1.png";
+function Card() {
+  return <div>Card</div>;
+}
+
+export default Card;
--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -1,3 +1,18 @@
+import Card from "./Card";
+import { MdAdd } from "react-icons/md";
+
 export default function Dashboard() {
-  return <></>;
+  return (
+    <section className="grid grid-cols-12 place-items-center">
+      <div></div>
+      <div>
+        <Card />
+        <Card />
+      </div>
+      <button className="flex flex-col-reverse items-center justify-center gap-2 text-blue-600 bg-blue-100/70 p-2 py-3 rounded-md">
+        <MdAdd size={20} />
+        <span className="write-vertical text-xl">Add Card</span>
+      </button>
+    </section>
+  );
 }
