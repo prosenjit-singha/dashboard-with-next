8fcdb1f47b7245ce80971fe950840c7e0aa6aa44
PJAheibam <prosenjitsingha28@gmail.com>
2023-02-09 21:57:41 +0600

    transaction history section completed

--- components/Dashboard/TransactionHistory/Table/TableRow.tsx
+++ components/Dashboard/TransactionHistory/Table/TableRow.tsx
@@ -6,8 +6,15 @@ type Props = {
   data: Transaction;
 };
 
-function TableData({ children }: { children: React.ReactNode }) {
-  return <td className="capitalize p-2">{children}</td>;
+function TableData({
+  children,
+  ...rest
+}: { children: React.ReactNode } & React.ComponentProps<"td">) {
+  return (
+    <td className="capitalize p-2" {...rest}>
+      {children}
+    </td>
+  );
 }
 
 export function TableRow({ data }: Props) {
@@ -23,10 +30,26 @@ export function TableRow({ data }: Props) {
     transactionType,
   } = data;
   return (
-    <tr className="hover:text-gray-500">
+    <tr className="hover:text-gray-500 hover:bg-slate-100 rounded">
       <TableData>#{id}</TableData>
-      <TableData>{name}</TableData>
-      <TableData>{price}</TableData>
+      <TableData className="flex gap-2">
+        {/* avatar */}
+        <div className="my-auto min-w-[40px] min-h-[40px] rounded bg-[#9E9E9E]">
+          {/* put image here */}
+        </div>
+        {/* name */}
+        <div className="flex flex-col my-2">
+          <span className="block text-sm leading-3">
+            {transactionType === "income" ? "Received" : "Transfer To"}
+          </span>
+          <span className="text-md text-[#1A1A1A]">{name}</span>
+        </div>
+      </TableData>
+      <TableData className="text-[#1A1A1A]">
+        {transactionType === "income" ? "+" : "-"}
+
+        {price}
+      </TableData>
       <TableData>{"Aug 02, 2023"}</TableData>
       <TableData>{category}</TableData>
       <TableData>{paymentMethod}</TableData>
